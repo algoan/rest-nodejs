a4895926bd09f5a1885979a710f0ab0551792720
Charles Coeurderoy <charles.coeurderoy@algoan.com>
2020-06-16 11:21:09 +0200

    chore(coverage): add jest coverage configurations

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/dist/']
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0,
+    },
+  },
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/node_modules/**', '!src/lib/index.ts'],
 };

2f1d4dbd88995302c97bd6e8937f3eb69c2d894e
Salim Ben Dakhlia <bendakhlia.salim@gmail.com>
2022-09-21 16:06:07 +0200

    feat: update the service account model with the widget configuration

---
+++ src/core/IFrameConfig.ts
@@ -0,0 +1,19 @@
+/**
+ * The Iframe configuration
+ */
+export class IFrameConfig {
+    /**
+     * The language of the iframe
+     */
+    public language?: string;
+   
+    /**
+     * The font used in the iframe
+     */
+    public font?: string;
+
+    /**
+     * The color of the font used in the iframe
+     */
+    public fontColor?: string;
+}
---
+++ src/core/WidgetConfig.ts
@@ -0,0 +1,11 @@
+import { IFrameConfig } from "./IFrameConfig";
+
+/**
+ * Service account's widget config class
+ */
+export class WidgetConfig {
+    /**
+     * The iframe configuration
+     */
+    public iframe?: IFrameConfig;
+}
